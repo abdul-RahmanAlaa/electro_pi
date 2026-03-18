6ed32cfcdccc790d73589c302c717534fe258d3e
Abdulrhman Alaa <abdoalaaabdo17@gmail.com>
2026-03-18 16:25:31 +0200

    Remove output option from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
 
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'cdn.dummyjson.com' }],
